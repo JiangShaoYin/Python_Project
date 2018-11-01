966a24473ec5a893b611809c3e4aa721e4eaf50a
JiangShaoYin <tooo_cold@163.com>
2018-11-01 13:32:54 +0800

    1

---
+++ a.py
@@ -0,0 +1,12 @@
+#!   /usr/bin/python
+#    coding:utf-8
+#	 @file    a.py
+#	 @author  Sean(jiangshaoyin@pku.edu.cn)
+#	 @date    2018-11-01 12:53:42
+ 
+ 
+import tensorflow as tf
+import numpy as np
+
+import os
+print (os.path.abspath('.'))
